13922a89f5715a3557dc52d1b6c149ff8a60c879
unknown <gennadii.itdeveloper@gmail.com>
2024-03-09 00:45:34 +0100

    tstlaravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>IT Kung-fu & Laravel</title>
+</head>
+<body>
+
+<h3>Test Kung-fu</h3>
+
+<p><a href="{{ route('page1') }}">Link to another page</a></p>
+
+@php
+
+echo "Test PHP echo through @php"
+
+@endphp
+
+
+
+
+</body>
+</html>
+
+
+
+
---
+++ resources/views/page/page1.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>IT Kung-fu & Laravel</title>
+</head>
+<body>
+
+<h3>Link Works </h3>
+
+<p><a href="{{ route('index') }}">Back</a></p>
+
+</body>
+
+
+
+
+
