b6c4e5e0a18ea67b44415859379722ae4a69891b
unknown <gennadii.itdeveloper@gmail.com>
2024-03-13 22:21:31 +0100

    Add AJAX and SSE and WebH-5

--- app/Http/Controllers/SSEController.php
+++ app/Http/Controllers/SSEController.php
@@ -3,37 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SSEController extends Controller
 {
     public function index()
     {
-        // Отправляем заголовки для SSE
-        $response = response()->stream(
-            function () {
-                $counter = 1;
-                while (true) {
-                    try {
-                        $message = 'Log ' . $counter;
-                        echo "data: $message\n\n";
-                        $counter++;
-                        sleep(1); // Задержка в 1 секунду
-                    } catch (\Throwable $e) {
-                        // Обработка исключений
-                        echo "data: Exception occurred: " . $e->getMessage() . "\n\n";
-                    }
-                }
-            },
-            200,
-            [
-                'Content-Type' => 'text/event-stream',
-                'Cache-Control' => 'no-cache',
-                'Connection' => 'keep-alive',
-            ]
-        );
+        $response = new StreamedResponse(function () {
+            $counter = 0;
+            while (true) {
+                echo "data: Log " . ++$counter . "\n\n";
+                ob_flush();
+                flush();
+                sleep(1);
+            }
+        });
 
-        ob_flush(); // Очистить буфер вывода
-        flush();    // Очистить вывод
+        $response->headers->set('Content-Type', 'text/event-stream');
+        $response->headers->set('Cache-Control', 'no-cache');
+        $response->headers->set('Connection', 'keep-alive');
 
         return $response;
     }
