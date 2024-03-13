3e6b439e68dfbe80c6ddcc5caf398c1f1528cbe1
unknown <gennadii.itdeveloper@gmail.com>
2024-03-13 22:33:08 +0100

    Add AJAX and SSE and WebH-6(if-else)

--- app/Http/Controllers/SSEController.php
+++ app/Http/Controllers/SSEController.php
@@ -9,23 +9,31 @@ class SSEController extends Controller
 {
     public function index(): StreamedResponse
     {
-        $response = new StreamedResponse(function () {
-            $counter = 0;
-            while (true) {
-                echo "data: Log " . ++$counter . "\n\n";
-                // Отправляем данные на клиент
-                ob_flush();
-                flush();
-                // "Ждем" 1 секунду перед отправкой следующего сообщения
-                sleep(1);
-            }
-        });
-
-        // Устанавливаем необходимые заголовки для SSE
-        $response->headers->set('Content-Type', 'text/event-stream');
-        $response->headers->set('Cache-Control', 'no-cache');
-        $response->headers->set('Connection', 'keep-alive');
-
-        return $response;
+        return response()->stream(
+            function () {
+                $counter = 0;
+                while (true) {
+                    echo "data: Log " . ++$counter . "\n\n";
+                    // Отправляем данные на клиент
+                    if (function_exists('fastcgi_finish_request')) {
+                        // Завершаем текущий запрос, чтобы данные были отправлены независимо от дальнейшей обработки
+                        fastcgi_finish_request();
+                    } else {
+                        // Для серверов без fastcgi_finish_request
+                        ob_flush();
+                        flush();
+                    }
+                    // "Ждем" 1 секунду перед отправкой следующего сообщения
+                    sleep(1);
+                }
+            },
+            200,
+            [
+                'Content-Type' => 'text/event-stream',
+                'Cache-Control' => 'no-cache',
+                'Connection' => 'keep-alive',
+            ]
+        );
     }
 }
+
