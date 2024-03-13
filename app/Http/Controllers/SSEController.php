0bb185d375cfaa768302c2a2d51e62ce94a59a02
unknown <gennadii.itdeveloper@gmail.com>
2024-03-13 22:30:31 +0100

    Add AJAX and SSE and WebH-6

--- app/Http/Controllers/SSEController.php
+++ app/Http/Controllers/SSEController.php
@@ -7,18 +7,21 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SSEController extends Controller
 {
-    public function index()
+    public function index(): StreamedResponse
     {
         $response = new StreamedResponse(function () {
             $counter = 0;
             while (true) {
                 echo "data: Log " . ++$counter . "\n\n";
+                // Отправляем данные на клиент
                 ob_flush();
                 flush();
+                // "Ждем" 1 секунду перед отправкой следующего сообщения
                 sleep(1);
             }
         });
 
+        // Устанавливаем необходимые заголовки для SSE
         $response->headers->set('Content-Type', 'text/event-stream');
         $response->headers->set('Cache-Control', 'no-cache');
         $response->headers->set('Connection', 'keep-alive');
